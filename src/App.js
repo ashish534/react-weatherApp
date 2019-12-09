03b909b956f6420eb539361b6f9a089ee410d725
ashishchauhan <achauhan@grepruby.io>
2019-12-09 09:55:58 +0530

    updted app.js as per logic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
+import Form from './form';
+import Weather from './weather';
+import Titles from './title';
+// import logo from './logo.svg';
 import './App.css';
+const divStyle = {
+  color: 'blue'
+};
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  state = {
+    temperature_min: undefined,
+    temperature_max: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
+  getWeather = async (e) => {
+    const city = e.target.elements.city.value;
+
+    const country = e.target.elements.country.value;
+    e.preventDefault();
+    const Api_Key = 'c0dbc8a405139cdea9f4ebde83363718';
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=metric&appid=${Api_Key}`);
+
+    const response = await api_call.json();
+
+    console.log(response);
+    if (city && country && response.cod === 200) {
+      this.setState({
+        temperature_min: response.main.temp_min,
+        temperature_max: response.main.temp_max,
+        city: response.name,
+        country: response.sys.country,
+        humidity: response.main.humidity,
+        description: response.weather[0].description,
+        error: ""
+      })
+    } else {
+      this.setState({
+        error: "Please make sure you are giving correct city name and country name"
+      })
+    }
+
+  }
+  render() {
+    return (
+      <div style={divStyle} >
+        <Titles />
+        <Form loadWeather={this.getWeather} />
+          <Weather
+            temperature_min={this.state.temperature_min}
+            temperature_max={this.state.temperature_max}
+            city={this.state.city}
+            country={this.state.country}
+            humidity={this.state.humidity}
+            description={this.state.description}
+            error={this.state.error}
+          />
+      </div>
+    );
+  }
 }
 
 export default App;
